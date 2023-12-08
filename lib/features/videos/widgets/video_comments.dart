2923e17d8e60139af40fe9d11f9d953083a84be0
moonk <mkjoehsb@gmail.com>
2023-12-08 11:16:14 +0900

    comments with bottomNavigationBar

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -17,30 +18,93 @@ class _VideoCommentsState extends State<VideoComments> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(Sizes.size14),
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(Sizes.size14),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text("22796 comments"),
+          actions: [
+            IconButton(
+              onPressed: _onClosePressed,
+              icon: const FaIcon(
+                FontAwesomeIcons.xmark,
+              ),
+            ),
+          ],
         ),
-        child: Scaffold(
-          backgroundColor: Colors.grey.shade100,
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: const Text("22796 comments"),
-            actions: [
-              IconButton(
-                onPressed: _onClosePressed,
-                icon: const FaIcon(
-                  FontAwesomeIcons.xmark,
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v40,
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text(
+                  "문경",
                 ),
               ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "문경",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500),
+                    ),
+                    Gaps.v3,
+                    const Text(
+                      "That's not ie I've seen the same thing but also i",
+                    ),
+                  ],
+                ),
+              ),
+              Column(children: [
+                FaIcon(
+                  FontAwesomeIcons.heart,
+                  size: Sizes.size20,
+                  color: Colors.grey.shade500,
+                ),
+                Gaps.v5,
+                Text("52.2K",
+                    style: TextStyle(
+                      color: Colors.grey.shade400,
+                    )),
+              ]),
             ],
           ),
-          body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) => Container(
-              child: const Text("I'm a comment"),
-            ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                child: const Text(
+                  "문경",
+                ),
+              ),
+              Gaps.h10,
+              const Expanded(
+                child: TextField(),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
