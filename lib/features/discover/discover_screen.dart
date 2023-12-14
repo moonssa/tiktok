c889b4898337405449e8c5a5ce89b9ac28989ca8
moonk <mkjoehsb@gmail.com>
2023-12-14 10:19:08 +0900

    tab bar in AppBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/sizes.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+final tabs = ["Top", "Users", "Videos", "Sounds", "LIVE", "Shopping", "Brands"];
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
         appBar: AppBar(
-      title: const Text('Discover'),
-    ));
+          title: const Text('Discover'),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            tabAlignment: TabAlignment.start,
+            isScrollable: true,
+            // padding: const EdgeInsets.symmetric(
+            //   horizontal: Sizes.size32,
+            // ),
+            unselectedLabelColor: Colors.grey.shade500,
+            labelColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+            indicatorColor: Colors.black,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size48,
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/features/discover/discover_screen.dart';
 import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok/features/videos/video_timeline_screen.dart';
@@ -13,7 +14,7 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
 
   final screens = [
     Container(),
@@ -54,7 +55,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
           Offstage(
             offstage: _selectedIndex != 1,
-            child: Container(),
+            child: const DiscoverScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 3,
